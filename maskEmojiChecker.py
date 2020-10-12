20ae307ff8bb88a4b6821f6ae345e8f4d542c5ea
Daniel <DanielKJackson95@gmail.com>
2020-10-11 20:19:44 -0400

    a

--- maskEmojiChecker.py
+++ maskEmojiChecker.py
@@ -11,10 +11,10 @@ BEARER_TOKEN = os.getenv('BEARER_TOKEN')
 print("Connecting")
 # 😷
 
+headers = {
+    'Authorization': BEARER_TOKEN,
+}
 
-# headers = {
-#     'Authorization': 'Bearer AAAAAAAAAAAAAAAAAAAAADcsIgEAAAAAy1qlVnUMfKAGBV5UQ2h1V7PdliM%3DZmAnxnBehVs1NFQN3Y4gbgpAzYS029WX8J8bBK2YBEGhM3KpyD',
-# }
 # response = requests.get('https://api.twitter.com/2/tweets/sample/stream', headers=headers)
 # print(response)
 print("Finished")
